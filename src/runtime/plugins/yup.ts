c9a76f68593ea866998885c484ca014f79b30712
DarlanPrado <darlandoprado2014@gmail.com>
2025-01-18 23:28:53 -0300

    feat: created configuration to customize yup via appconfig

--- src/runtime/plugins/yup.ts
+++ src/runtime/plugins/yup.ts
@@ -1,5 +1,5 @@
 import * as yup from 'yup'
-import { defineNuxtPlugin } from '#app'
+import { defineNuxtPlugin, useAppConfig } from '#app'
 
 declare module '#app' {
   interface NuxtApp {
@@ -7,8 +7,24 @@ declare module '#app' {
   }
 }
 
+interface YupSchema {
+  setLocale?: yup.LocaleObject[]
+}
+
+declare module 'nuxt/schema' {
+  interface AppConfigInput {
+    yup?: YupSchema
+  }
+}
+
 export default defineNuxtPlugin((_nuxtApp) => {
-  console.log('Plugin injected by my-module!')
+  const yupAppConfig = useAppConfig().yup as YupSchema || null
+
+  if (yupAppConfig?.setLocale?.length) {
+    yupAppConfig.setLocale!.forEach((data) => {
+      yup.setLocale(data)
+    })
+  }
 
   _nuxtApp.$yup = yup
 })
